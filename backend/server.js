2d2936799481785cbf50ff09f96ee32d99806cf6
sazid62 <sajidurrahman3s2@gmail.com>
2024-11-29 12:17:43 +0600

    project completed

--- backend/server.js
+++ backend/server.js
@@ -11,7 +11,7 @@ const app = express();
 
 
 const __dirname = path.resolve();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 5000;
 app.use(express.json());
 app.use("/products", productsRoutes);
 
